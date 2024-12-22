060d12c58073ae452f3d839f83192221a584b4a3
Minhajul Anwar <minhaj.me.bd@gmail.com>
2024-12-22 16:17:22 +0600

    breaking news section px-4

--- components/home/BreakingNews.tsx
+++ components/home/BreakingNews.tsx
@@ -19,24 +19,26 @@ const BreakingNews = () => {
     if (isLoading) return <div></div>;
 
     return (
-        <div className="container mx-auto my-2">
-            <div className="row">
-                <div className="col-md-12">
-                    <div className="d-flex justify-content-between align-items-center breaking-news bg-white">
-                        <div className="d-flex justify-content-center align-items-center py-2 text-white px-1 scroller-title">
-                            <span>শিরোনাম</span>
-                        </div>
-                        <div className="marquee-wrapper">
-                            <div className="marquee">
-                                {data.map((entry: any) => {
-                                    return <a className="mx-6" key={entry.news_title} href={"javascript:void(0)"}>{entry.news_title}</a>
-                                })}
+        <section>
+            <div className="container px-4 mx-auto my-2">
+                <div className="row">
+                    <div className="col-md-12">
+                        <div className="d-flex justify-content-between align-items-center breaking-news bg-white">
+                            <div className="d-flex justify-content-center align-items-center py-2 text-white px-1 scroller-title">
+                                <span>শিরোনাম</span>
+                            </div>
+                            <div className="marquee-wrapper">
+                                <div className="marquee">
+                                    {data.map((entry: any) => {
+                                        return <a className="mx-6" key={entry.news_title} href={"javascript:void(0)"}>{entry.news_title}</a>
+                                    })}
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </section>
     );
 }
 
