3ad2db95d7c545063aa7bcdaf6e84cc4512bc699
Minhajul Anwar <minhaj.me.bd@gmail.com>
2024-12-22 16:52:48 +0600

    dark theme button hide

--- components/navBar/NavBar.tsx
+++ components/navBar/NavBar.tsx
@@ -114,6 +114,7 @@ const NavBar = () => {
             <div className="flex items-center justify-center print:hidden">
               <div className="p-3 last:pr-0 hidden md:block">
                 <button
+                  style={{display: 'none'}}
                   className="flex"
                   aria-label="theme"
                   onClick={handleTheme}
