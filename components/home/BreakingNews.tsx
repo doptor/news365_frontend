6749cda2ec2b0bdb5d87ce064dfda239777b5c77
Minhajul Anwar <minhaj.me.bd@gmail.com>
2024-12-29 19:48:39 +0600

    fix: marquee pause not working

--- components/home/BreakingNews.tsx
+++ components/home/BreakingNews.tsx
@@ -21,8 +21,11 @@ const BreakingNews = () => {
     if (isLoading) {
         return <div></div>;
     } else {
-        marquee_dur = data.length * 3;
+        marquee_dur = data.length * 4;
     }
+
+    // const marquee_style = {animation: "marquee " + marquee_dur.toString() + "s linear infinite"};
+    const marquee_style = {animationDuration: marquee_dur.toString() + 's'};
 
     return (
         <section>
@@ -34,7 +37,7 @@ const BreakingNews = () => {
                                 <span>শিরোনাম</span>
                             </div>
                             <div className="marquee-wrapper mx-3">
-                                <div className="marquee" style={{animation: "marquee " + marquee_dur.toString() + "s linear infinite"}}>
+                                <div className="marquee" style={marquee_style}>
                                     {data.map((entry: any) => {
                                         return <a className="mx-6" key={entry.news_title} href={"javascript:void(0)"}>{entry.news_title}</a>
                                     })}
