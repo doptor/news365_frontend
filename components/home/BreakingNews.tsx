2d41f6a22a71c20e98f7e6de421fb4bd7b79b739
Minhajul Anwar <minhaj.me.bd@gmail.com>
2025-01-19 17:13:05 +0600

    no marquee when data empty

--- components/home/BreakingNews.tsx
+++ components/home/BreakingNews.tsx
@@ -25,30 +25,34 @@ const BreakingNews = () => {
     }
 
     // const marquee_style = {animation: "marquee " + marquee_dur.toString() + "s linear infinite"};
-    const marquee_style = {animationDuration: marquee_dur.toString() + 's'};
+    const marquee_style = { animationDuration: marquee_dur.toString() + 's' };
 
-    return (
-        <section className="print:hidden">
-            <div className="container px-4 mx-auto my-1">
-                <div className="row">
-                    <div className="col-md-12">
-                        <div className="d-flex justify-content-between align-items-center breaking-news bg-white">
-                            <div className="d-flex justify-content-center align-items-center py-1 px-4 text-white scroller-title">
-                                <span>শিরোনাম</span>
-                            </div>
-                            <div className="marquee-wrapper mx-3">
-                                <div className="marquee" style={marquee_style}>
-                                    {data.map((entry: any) => {
-                                        return <a className="mx-6" key={entry.news_title} href={"javascript:void(0)"}>{entry.news_title}</a>
-                                    })}
+    if (data.length) {
+        return (
+            <section className="print:hidden">
+                <div className="container px-4 mx-auto my-1">
+                    <div className="row">
+                        <div className="col-md-12">
+                            <div className="d-flex justify-content-between align-items-center breaking-news bg-white">
+                                <div className="d-flex justify-content-center align-items-center py-1 px-4 text-white scroller-title">
+                                    <span>শিরোনাম</span>
+                                </div>
+                                <div className="marquee-wrapper mx-3">
+                                    <div className="marquee" style={marquee_style}>
+                                        {data.map((entry: any) => {
+                                            return <a className="mx-6" key={entry.news_title} href={"javascript:void(0)"}>{entry.news_title}</a>
+                                        })}
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </section>
-    );
+            </section>
+        );
+    } else {
+        return <></>;
+    }
 }
 
 export default BreakingNews;
