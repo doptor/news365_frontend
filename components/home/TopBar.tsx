6bcde7882118ebf7b878ddc373d9ae17a37497cc
Minhajul Anwar <minhaj.me.bd@gmail.com>
2025-01-21 14:15:48 +0600

    social media megamenu

--- components/home/TopBar.tsx
+++ components/home/TopBar.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import {WebSettingContext} from "@/context/webSettingContext";
-import {useContext, useEffect, useState} from "react";
+import { WebSettingContext } from "@/context/webSettingContext";
+import { useContext, useEffect, useState } from "react";
 import "moment/locale/bn";
 
 import Socials from "../common/socials/Socials";
@@ -10,14 +10,18 @@ import "@/app/datebar.css";
 import Link from "next/link";
 import Image from "next/image";
 
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
     faTableList,
     faNewspaper,
     faBoxArchive as faArchive,
     faThumbsUp,
     faTv,
+    IconDefinition,
+
 } from '@fortawesome/free-solid-svg-icons';
+
+import { faFacebookF, faYoutube, faXTwitter, faTiktok, faLinkedin, faInstagram, faYoutubeSquare, faTwitterSquare, faFacebookSquare, faLinkedinIn, faInstagramSquare } from '@fortawesome/free-brands-svg-icons';
 
 const TopBar = () => {
 
@@ -38,8 +42,8 @@ const TopBar = () => {
 
     if (webSettingData) {
         // const logo = webSettingData.logo;
-        const {logo, social_link} = webSettingData;
-        const {fb, flickr, google, linkd, pin, tw, vimo, vk, youtube} = social_link;
+        const { logo, social_link } = webSettingData;
+        const { fb, tw, linkd, insta, youtube, tiktok } = social_link;
         const topLinks: any[] = [
             {
                 label: "ই পেপার",
@@ -48,12 +52,15 @@ const TopBar = () => {
                 href: "https://epaper.dailyniropekkho.com/",
                 subMenu: []
             },
-            {label: "আর্কাইভ", key: "archive", icon: faArchive, href: "javascript:void(0)", subMenu: []},
+            { label: "আর্কাইভ", key: "archive", icon: faArchive, href: "javascript:void(0)", subMenu: [] },
             {
                 label: "সোশ্যাল মিডিয়া", key: "social", icon: faThumbsUp, href: "javascript:void(0)", subMenu: [
-                    youtube && {label: "youtube", href: youtube},
-                    fb && {label: "facebook", href: fb},
-                    tw && {label: "twitter", href: tw},
+                    fb && { label: "Facebook", icon: faFacebookSquare, href: fb.split(',') },
+                    youtube && { label: "Youtube", icon: faYoutubeSquare, href: youtube.split(',') },
+                    tiktok && { label: "Tiktok", icon: faTiktok, href: tiktok.split(',') },
+                    linkd && { label: "LinkedIn", icon: faLinkedinIn, href: linkd.split(',') },
+                    tw && { label: "X.com", icon: faTwitterSquare, href: tw.split(',') },
+                    insta && { label: "Instagram", icon: faInstagramSquare, href: insta.split(',') },
                 ].filter(Boolean)
             },
         ];
@@ -62,12 +69,12 @@ const TopBar = () => {
             <div className="timebar border-b py-2 ">
                 <div className="container px-4 py-2 mx-auto flex justify-between">
                     <div>
-                        <Link href="/" aria-label="logo"> <Image src={logo} alt="logo" width={180} height={100}/></Link>
+                        <Link href="/" aria-label="logo"> <Image src={logo} alt="logo" width={180} height={100} /></Link>
                     </div>
-                    <div className="flex flex-wrap grow items-center justify-left ml-40 print:hidden">
+                    <div className="flex flex-wrap grow items-center justify-center print:hidden">
                         <ul className="flex gap-2 whitespace-nowrap">
                             {topLinks.map((item) => {
-                                const {label, key, href} = item;
+                                const { label, key, href } = item;
                                 let li_class = "";
                                 if (item.subMenu.length) li_class += " relative ";
                                 if (key !== 'epaper') li_class += " hidden sm:flex ";
@@ -77,20 +84,30 @@ const TopBar = () => {
                                         onMouseLeave={() => setOpenSubMenu(null)}
                                     >
                                         <Link className="flex items-center gap-1 py-[1px] px-3 text-md " href={href}>
-                                            <div className="font-bold"><FontAwesomeIcon icon={item.icon}/> {label}</div>
+                                            <div className="font-bold"><FontAwesomeIcon icon={item.icon} /> {label}</div>
                                         </Link>
                                         {item.key === openSubMenu ?
                                             <div
-                                                className="absolute left-0 top-full pt-2 w-48 bg-white border rounded shadow-md z-50"
-                                                onMouseLeave={() => setOpenSubMenu(null)}> {/* Mega menu */}
-                                                <ul className="list-none p-2">
-                                                    {item.subMenu.map((subItem: { label: string, href: string }) => (
-                                                        <li key={subItem.label}>
-                                                            <Link href={subItem.href}
-                                                                  className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">{subItem.label}</Link>
-                                                        </li>
+                                                className="absolute -right-80 top-full pt-2 w-48z bg-white border rounded shadow-md z-50"
+                                            onMouseLeave={() => setOpenSubMenu(null)}
+                                            > {/* Mega menu */}
+                                                <div className=" p-2">
+                                                    {item.subMenu.map((subItem: { label: string, icon: IconDefinition, href: string[] }) => (
+                                                        <div key={subItem.label} className="px-2">
+                                                            <div>{subItem.label}</div>
+                                                            <div className="flex">
+                                                                {subItem.href.map((url: string) => {
+                                                                    const urlParts = url.split('/').filter((part) => part);
+                                                                    const name = urlParts[urlParts.length - 1];
+                                                                    return <div key={url}>
+                                                                        <Link href={url} className="block px-4 py-2 text-xs text-gray-700 hover:bg-gray-100">
+                                                                            <span className="mr-2"><FontAwesomeIcon icon={subItem.icon} /></span>{name}</Link>
+                                                                    </div>
+                                                                })}
+                                                            </div>
+                                                        </div>
                                                     ))}
-                                                </ul>
+                                                </div>
                                             </div>
                                             : null
                                         }
@@ -99,11 +116,11 @@ const TopBar = () => {
                             })}
                         </ul>
                     </div>
-                    <div className="hidden lg:flex items-center justify-center print:hidden" style={{color: "#f04130 !important"}}>
+                    <div className="hidden lg:flex items-center justify-center print:hidden" style={{ color: "#f04130 !important" }}>
                         <Link className="flex items-center gap-1 py-[1px] px-3 text-md " href={youtube}>
                             {/* <div className="font-bold"><FontAwesomeIcon icon={faTv}/> লাইভ টিভি</div> */}
                             <button className="whitespace-nowrap rounded-3xl bg-[#f04130] text-white py-1.5 px-4 text-sm">
-                                <FontAwesomeIcon icon={faTv}/> লাইভ টিভি
+                                <FontAwesomeIcon icon={faTv} /> লাইভ টিভি
                             </button>
                         </Link>
                     </div>
